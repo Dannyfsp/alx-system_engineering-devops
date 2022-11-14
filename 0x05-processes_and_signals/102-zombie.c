1491a26cc84e88cdb7125b2e7234a372f93d760b
Dannyfsp <femoscopilala@gmail.com>
2022-11-13 18:12:00 -0800

    advanced - zombie PID

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -30,7 +30,7 @@ int main(void)
 		zombie = fork();
 		if (!zombie)
 			return (0);
-		printf("Zombie process created, PID: %d\n", zombie)
+		printf("Zombie process created, PID: %d\n", zombie);
 	}
 	infinite_while();
 	return (0);
