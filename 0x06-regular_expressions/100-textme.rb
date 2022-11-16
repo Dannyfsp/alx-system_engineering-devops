d0c153ee1657caac9ce412e93737ee8d2eab5dae
Dannyfsp <femoscopilala@gmail.com>
2022-11-15 17:58:07 -0800

    A regular expression that matches a particular pattern of texts

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# A regular expression that matches a particualr pattern of texts
+
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
